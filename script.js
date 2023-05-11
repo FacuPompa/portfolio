a0c0b603d61b8afa57d6b24e7b7d7543adb19b70
Facundo Pompa <pompafacundo@gmail.com>
2023-05-11 20:18:39 -0300

    correccion js y css

--- script.js
+++ script.js
@@ -1,33 +1,28 @@
-//Función que me aplica el estilo a la opciòn seleccionada y quita la previamente seleccionada
+//función que me aplica el estilo a la opción seleccionada y quita la previamente seleccionada
 function seleccionar(link) {
-    let opciones = document.querySelectorAll('#links  a');
-    opciones[0].className = "";
-    opciones[1].className = "";
-    opciones[2].className = "";
-    opciones[3].className = "";
-    opciones[4].className = "";
-    link.className = "seleccionado";
+    let opciones = document.querySelectorAll('#links a');
+    opciones.forEach(opcion => opcion.classList.remove("seleccionado"));
+    link.classList.add("seleccionado");
 
-    //Hacemos desaparecer el menu una vez que se ha seleccionado una opcion
-    //en modo responsive
+    //hacemos desaparecer el menú una vez que se ha seleccionado una opción en modo responsive
     let x = document.getElementById("nav");
-    x.className = "";
+    x.classList.remove("responsive");
 }
 
-//función que muestra el menu responsive
+//función que muestra el menú responsive
 function responsiveMenu() {
-    let x = document.getElementById("nav");
-    if (x.className === "") {
-        x.className = "responsive";
-    } else {
-        x.className = "";
-    }
+    toggleMenu();
 }
 
-//detecto el scrolling para aplicar la animación del la barra de habilidades
+function toggleMenu() {
+    let nav = document.getElementById("nav");
+    nav.classList.toggle("responsive");
+}
+
+//detecto el scrolling para aplicar la animación de la barra de habilidades
 window.onscroll = function() { efectoHabilidades() };
 
-//funcion que aplica la animación de la barra de habilidades
+//función que aplica la animación de la barra de habilidades
 function efectoHabilidades() {
     let skills = document.getElementById("skills");
     let distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
@@ -36,5 +31,46 @@ function efectoHabilidades() {
         document.getElementById("js").classList.add("barra-progreso2");
         document.getElementById("bd").classList.add("barra-progreso3");
     }
+}
 
+//función para descargar el CV al hacer clic en el botón
+document.getElementById('downloadbtn').addEventListener('click', function() {
+    downloadCV();
+});
+
+function downloadCV() {
+    let link = document.createElement('a');
+    link.href = '/utils/cv.pdf';
+    link.download = 'facundo_pompa_cv.pdf';
+
+    link.click();
+}
+
+//función para mostrar una alerta al enviar el formulario
+function mostrarAlerta(titulo, mensaje, tipo) {
+    alert(titulo + ": " + mensaje);
+}
+
+//función para enviar el formulario
+function sendForm(event) {
+    event.preventDefault();
+
+    let formulario = document.getElementById("form");
+    let nombreCompleto = formulario.elements["name"].value;
+    let email = formulario.elements["email"].value;
+    let tema = formulario.elements["title"].value;
+    let mensaje = formulario.elements["message"].value;
+
+    if (nombreCompleto.trim() === "" || email.trim() === "" || tema.trim() === "" || mensaje.trim() === "") {
+        mostrarAlerta("Error", "Por favor, completa todos los campos obligatorios.", "error");
+        return;
+    }
+
+    if (!validarFormatoEmail(email)) {
+        mostrarAlerta("Error", "Por favor, ingresa una dirección de correo electrónico válida.", "error");
+        return;
+    }
+
+    // Envía el formulario
+    formulario.submit();
 }
